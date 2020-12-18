81ec6e4fc4a5d133f91a99283449c9cbcc411fa1
Sauvik Roy <sauvik3@gmail.com>
2020-12-18 16:21:33 +0530

    Fix Segmentation Error on Linux system

--- Sorting/src/Utils.cpp
+++ Sorting/src/Utils.cpp
@@ -3,20 +3,26 @@
 #include <memory>
 #include "Utils.h"
 
-void print_array(int *input_array, int length) {
+void print_array(int *input_array, int length)
+{
+	LOG("%s", "[");
 	for (auto i = 0; i < length; ++i) {
 		LOG("%d%s", input_array[i], (i == length - 1 ? "" : ", "));
 	}
+	LOG("%s", "]");
 }
 
 void logger::log(const char * format, ...) const
 {
-	va_list args;
-	va_start(args, format);
+	va_list args1, args2;
+	va_start(args1, format);
+    	va_copy(args2, args1);
 
-	const auto length = vsnprintf(nullptr, 0, format, args) + 1;
+	const auto length = vsnprintf(nullptr, 0, format, args1) + 1;
+	va_end(args1);
+
 	auto message = std::make_unique<char[]>(length);
-	vsnprintf(message.get(), length, format, args);
+	vsnprintf(message.get(), length, format, args2);
 	printf("%s", message.get());
-	va_end(args);
+	va_end(args2);
 }
